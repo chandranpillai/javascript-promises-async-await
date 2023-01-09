fdd8140bf821c5b35c6efaa47c8e36d3eb93dd83
Chandran Pillai <cpillai@smartcommunications.com>
2023-01-09 21:39:44 +0000

    Completed module 3

--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import {fetchWithTimeout} from "./services";
 const movies = require("./data/movies.json")
 
 export function fetchMovies() {
-
-}
-export function fetchMovies() {
     const resolveFunction = () => movies;        
     return fetchWithTimeout(1000)
         .then(resolveFunction);
  }
  
  const moviePromise = fetchMovies();
- moviePromise.the(results => {
+ moviePromise
+    .the(results => {
     console.log(results);
  })
  
