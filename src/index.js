b8b02332e4ba8ec5229650cbc0c3503cd5670d71
Chandran Pillai <cpillai@smartcommunications.com>
2023-01-09 21:07:09 +0000

    Completed module 3

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
+import { result } from "lodash";
+import { fetchWithTimeout } from "./services";
+
 fetchMovies();
-export function fetchMovies() { }
+export function fetchMovies() {
+    let movies = require ("./data/movies.json")
+    const resolveFunction = movies();
+    fetchWithTimeout(1000)
+    .then(resolveFunction);
+ }
+ 
+ const moviePromise = new Promise(function(results) {
+    fetchMovies();
+ 
+ moviePromise()
+    .then(Promise.resolve(results))
+    console.log(results)
+ })
