43f84e688fea18a6dd31da860dcdac8391fbeb08
Chandran Pillai <cpillai@smartcommunications.com>
2023-01-09 20:13:18 +0000

    Completed module 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
-function fetchMovies() {
-
-}
+fetchMovies();
+function fetchMovies() { }
--- src/services.js
+++ src/services.js
@@ -1,3 +1,4 @@
+fetchWithTimeout();
 let delay;
 function fetchWithTimeout(delay) {
     return new Promise(resolve => setTimeout(resolve, delay));
