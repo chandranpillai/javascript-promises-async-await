0f862cb31dd8846a1766151ba1a69b14a4fb08bb
Chandran Pillai <cpillai@smartcommunications.com>
2023-01-10 01:19:27 +0000

    Completed module 7

--- src/index.js
+++ src/index.js
@@ -44,13 +44,12 @@ async function getBooksAndMoviesAsync() {
 }
 async function getBooksOrMoviesAsync() {
     try {
-        const values = await Promise.race([asyncFetchBooks(), asyncFetchMovies()])
+        const values = await Promise.race([asyncFetchBooks(), asyncFetchMovies()]);
         return values;
 
     }
     catch(error) {
         console.error("Error waiting for the promise race", error);
-
     }
 
 }
@@ -61,13 +60,14 @@ getBooksAndMoviesAsync()
     movies: results.movies,
     books: results.books,
   });
-});
+})
 
 getBooksOrMoviesAsync()
 .then(results => {
     console.log("movies OR books", {
-        results,
+        results
     });
-});
+})
+.catch(err => console.error(err));
 
 
